299604d79d2c4eb2d257c6baca9ddd8c72337b04
Ryan Florence <rpflorence@gmail.com>
2015-03-08 14:50:15 -0600

    handle submit on form submit, not button clicks

--- app/AddList.js
+++ app/AddList.js
@@ -19,6 +19,7 @@ var AddList = React.createClass({
     });
   },
   handleSubmit: function(e){
+    event.preventDefault();
     this.props.add(this.state);
     this.setState({
       listName: '',
@@ -35,14 +36,14 @@ var AddList = React.createClass({
       }
     }
     return (
-      <div className="col-sm-6">
+      <form className="col-sm-6" onSubmit={this.handleSubmit}>
         <h3 className="text-center"> Create New List </h3>
         List Name:
         <input type="text" placeholder="List Name" className="form-control" value={this.state.listName} onChange={this.handleChange} /> <br />
         List Background Color: <span style={styles.colorIndicator}></span>
         <ColorList chooseColor={this.chooseBackground} /> <br />
-        <button className="btn btn-primary" onClick={this.handleSubmit} > Submit </button>
-      </div>
+        <button type="submit" className="btn btn-primary"> Submit </button>
+      </form>
     )
   }
 });
