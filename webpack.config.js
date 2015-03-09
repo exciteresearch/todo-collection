becb0258aec4a1f590170fcf4f24d15799ae119d
Ryan Florence <rpflorence@gmail.com>
2015-03-09 12:26:06 -0600

    added webpack-dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   entry: "./app/App.js",
   output: {
-    filename: "public/bundle.js"
+    path: './public',
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
     loaders: [
